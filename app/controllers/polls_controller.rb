6ab4e65339daee94559b312bd3bdcb5e7ba05f74
Edvina Ambrozeviciute <edvina.ambrozeviciute@freeagent.com>
2022-08-22 16:58:28 +0100

    Add CRUD

---
+++ app/controllers/polls_controller.rb
@@ -0,0 +1,49 @@
+class PollsController < ApplicationController
+  def index
+    @polls = Poll.all
+    end
+
+  def show
+    @poll = Poll.find(params[:id])
+  end
+
+  def new
+    @poll = Poll.new
+  end
+
+  def create
+    @poll = Poll.new(poll_params)
+
+    if @poll.save
+      redirect_to @poll
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @poll = Poll.find(params[:id])
+  end
+
+  def update
+    @poll = Poll.find(params[:id])
+
+    if @poll.update(poll_params)
+      redirect_to @poll
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @poll = Poll.find(params[:id])
+    @poll.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def poll_params
+      params.require(:poll).permit(:title, :start_time, :end_time)
+    end
+end
