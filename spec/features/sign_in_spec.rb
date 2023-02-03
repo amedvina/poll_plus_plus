7c65011e22d9822680e4a51bbc165f8930b91482
Edvina Ambrozeviciute <edvina.ambrozeviciute@freeagent.com>
2023-02-03 09:04:21 +0000

    Adds sign up and sign in links

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -8,8 +8,6 @@ RSpec.feature "SignIn", type: :feature do
   let(:user) { User.create(username: "test", password: "password", password_confirmation: "password") }
 
   it "allows the user to create an account with valid credentials" do
-    puts user.password 
-
     fill_in "Username", with: "test"
     fill_in "Password", with: "password"
     click_button "Sign In"
@@ -17,11 +15,15 @@ RSpec.feature "SignIn", type: :feature do
     expect(page).to have_content("Logged in successfully")
   end
 
-  it " does not allow the user to create an account with invalid credentials" do    
+  it "does not allow the user to create an account with invalid credentials" do    
     fill_in "Username", with: "test"
     fill_in "Password", with: "wrong_password"
     click_button "Sign In"
 
     expect(page).to have_content("Invalid username or password")
+  end
+
+  it "has links to sign up page" do
+    expect(page).to have_link("Haven't created an account yet? Sign up here", href: sign_up_path)
   end
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,21 +1,29 @@
 require "rails_helper"
 
 RSpec.feature "Sign up", type: :feature do
-  scenario "User signs up with valid information" do
+  before do
     visit sign_up_path
+  end
+
+  it "allows user to sign in with correct credentials" do
     fill_in "Username", with: "test"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button "Sign Up"
+    
     expect(page).to have_text("Successfully created account")
   end
 
-  scenario "User signs up with invalid information" do
-    visit sign_up_path
+  it "does allow user to sign in with correct credentials" do
     fill_in "Username", with: "test"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "pass"
     click_button "Sign Up"
+
     expect(page).to have_text("Password confirmation doesn't match Password")
+  end
+
+  it "has links to sign in page" do
+    expect(page).to have_link("Have an account? Sign in here", href: sign_in_path)
   end
 end
