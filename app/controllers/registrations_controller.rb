55e8a5fa9d696adec51a7c0be7f29f3c548204c4
Edvina Ambrozeviciute <edvina.ambrozeviciute@freeagent.com>
2023-03-24 12:35:08 +0000

    Disable sign ups on production

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,10 +2,17 @@ class RegistrationsController < ApplicationController
 	skip_before_action :require_user_logged_in!
 	
 	def new
+		if Rails.env.production?
+			render plain: "Not found", status: :not_found and return
+		end
 		@user = User.new
 	end
 
 	def create
+		if Rails.env.production?
+			render plain: "Not found", status: :not_found and return
+		end
+		
 		@user = User.new(user_params)
 		if @user.save
 			session[:user_id] = @user.id
